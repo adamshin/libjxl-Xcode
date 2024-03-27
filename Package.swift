2e161d047d6d4fef96fe0f034d638392e89f8cfb
Adam Shin <adamshin@users.noreply.github.com>
2024-03-26 21:05:17 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "libjxl",
     platforms: [
-        .macOS(.v10_11), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
+        .iOS(.v12),
     ],
     products: [
         .library(
